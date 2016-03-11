3da362ae634b03afd98bdb1783abccd96670e414
Javier Pimas <elpochodelagente@gmail.com>
2016-03-11 18:04:59 -0300

    group results by benchmark

--- metrika_reporter.py
+++ metrika_reporter.py
@@ -23,21 +23,33 @@ __author__ = 'Javier Pimás'
 
 
 def report(results):
-
     tab = tt.Texttable()
     tab.header(['Benchmark', 'Input', 'Average', 'std dev %', 'std dev', 'runs', 'discarded'])
 
-    tab.set_cols_align(['r','r', 'r','r', 'r', 'r', 'r'])
+    tab.set_cols_align(['r', 'r', 'r', 'r', 'r', 'r', 'r'])
     tab.set_deco(tab.HEADER | tab.VLINES)
 
-    for benchmark, measures in sorted(results.items()):
-        report_benchmark(tab, benchmark, measures)
+    grouped = {}
+
+    for benchmark in results.keys():
+        if not grouped.has_key(benchmark.name()):
+            grouped[benchmark.name()] = []
+
+        grouped[benchmark.name()].append(benchmark)
+
+    #for benchmark, measures in sorted(results.items()):
+    for group in grouped.values():
+        for benchmark in sorted(group):
+            measures = results[benchmark]
+            report_benchmark(tab, benchmark, measures)
+
+        tab.add_row(("··········", "······", "······", "·····", "·······", "·····", "······"))
 
     table = tab.draw()
     print (table)
 
     context = '\n%s-%s-%s on %s' % (platform.system(), platform.release(), platform.machine(),
-                                  time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())) + '.'
+                                    time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())) + '.'
 
     print (context)
     print ("%d logical cores (%d physical)" % (psutil.cpu_count(), psutil.cpu_count(False)))
@@ -47,19 +59,18 @@ def report(results):
 
 
 def report_benchmark(tab, benchmark, measures):
-
     trimmed = trim_ends(sorted(measures), 0.1)
     runs = len(trimmed)
     average = sum(trimmed) / runs
-    stddev  = sqrt(sum([(measure - average)**2 for measure in trimmed]))
+    stddev = sqrt(sum([(measure - average) ** 2 for measure in trimmed]))
     stddev_relative = stddev / average * 100
-    tab.add_row([benchmark.name(), benchmark.input, average, "%2.2f %%" % stddev_relative, stddev, len(measures), len(measures) - runs])
+    tab.add_row([benchmark.variation, benchmark.input, average, "%2.2f %%" % stddev_relative, stddev, len(measures),
+                 len(measures) - runs])
 
     tab.set_cols_width([35, 10, 10, 10, 10, 10, 10])
 
 
 def trim_ends(sorted_list, proportion_to_cut):
-
     size = len(sorted_list)
     left = int(proportion_to_cut * size)
     right = size - left
